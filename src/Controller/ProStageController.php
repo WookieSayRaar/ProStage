187f0cb091f7503513ded7939f88f3211024a86d
Antoine CHAMPAULT <antoine.champault@hotmail.com>
2021-01-09 10:36:46 +0100

    mise sur git

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -13,8 +13,36 @@ class ProStageController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('prostage/index.html.twig', [
+        return $this->render('prostage/acceuil.html.twig', [
             'controller_name' => 'ProStageController',
         ]);
     }
+
+    /**
+     * @Route("/formations", name="prostage_formations")
+     */
+     public function formation(): Response{
+     return $this->render('prostage/formation.html.twig', [
+         'controller_name' => 'ProStageController',
+     ]);
+   }
+
+
+     /**
+      * @Route("/entreprises", name="prostage_entreprises")
+      */
+      public function entreprise(): Response{
+      return $this->render('prostage/entreprise.html.twig', [
+          'controller_name' => 'ProStageController',
+      ]);}
+
+      /**
+       * @Route("/stage/{id}", name="prostage_stage")
+       */
+
+       public function stage($id): Response {
+       return $this->render('prostage/stage.html.twig', [
+           'controller_name' => 'ProStageController','idStage'=>$id
+       ]);}
 }
+?>
